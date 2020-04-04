cad0f78ba4b0343cf46f8865b485a0779c62fec5
Suetlivaya Ptichka <hawxbf@gmail.com>
2020-04-04 18:59:36 +0300

    Add rb02 task Change version to 0.3.2 Add 0.3.2 game ready

--- Scripts/BlueRotaryWingTasks.lua
+++ Scripts/BlueRotaryWingTasks.lua
@@ -11,13 +11,13 @@ rblue01_task =
     cancelTrigger = "40104",
     startMsgFriendly = [[NAME: "Apsheronsk insertion"
 
-OBJECTIVE: Pick up 2 groups from Warsaw FARP, drop them in EK62 square. Support them when they are on ground.
+OBJECTIVE: Pick up 2 groups from Warsaw FARP, drop them in EK62 square. Support them when they are on the ground.
 
-THREATS: Infantry, possible ZU-23
+THREATS: Infantry, possible zu-23.
 
 NAVIGATION: Fly heading 356 from Warsaw FARP, look for the powerlines.]],
     startMsgEnemy = "",
-    goodEndMsgFriendly = '"Apsheronks Insertion" complete!',
+    goodEndMsgFriendly = '"Apsheronks Insertion" completed!',
     goodEndMsgEnemy = "",
     badEndMsgFriendly = '"Apsheronks Insertion" failed, all troops are dead.',
     badEndMsgEnemy = "",
@@ -29,5 +29,36 @@ NAVIGATION: Fly heading 356 from Warsaw FARP, look for the powerlines.]],
     markText = '"Apsheronks Insertion" disembark point'
 }
 -------------------------------------------------------------------------------------------------------
+rblue02_task = 
+{
+    name = "Intercepting recovery",
+    coalition = 2,
+    startTrigger = "40201",
+    goodEndTrigger = "40202",
+    badEndTrigeer = "40203",
+    cancelTrigger = "40204",
+    startMsgFriendly = [[NAME: "Intercepting recovery"
+
+OBJECTIVE: Enemy helicopter crashed in square EK81. Soviet forces are trying to recovery helicopter crew. Find crash site and destroy enemy forces.
+
+THREATS: Infantry, soviet helicopters.
+
+NAVIGATION: Fly heading 032 from Warsaw FARP, look for the Dagestanskaya town.]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = '"Intercepting recovery" task completed!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = '"Intercepting recovery" failed, all troops are dead.',
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "rb02zone",
+    markText = '"Intercepting recovery" disembark point'
+}
+-------------------------------------------------------------------------------------------------------
+
+
 
 table.insert(blueRotaryWingTasksConfig, rblue01_task)
+table.insert(blueRotaryWingTasksConfig, rblue02_task)
